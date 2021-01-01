18e693ddd25b564fd58ca9bfc1daabccc59692de
Patrick R <me@patches.codes>
2021-01-01 07:45:23 -0800

    jsonified struct, added more flags, self clearing cache

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -14,9 +15,9 @@ import (
 )
 
 type record struct {
-	last_visit time.Time
-	uri        fasthttp.URI
-	key        string
+	Last_visit time.Time `json:"last_vist"`
+	Uri        string    `json:"uri"`
+	Key        string    `json:"key"`
 }
 
 func main() {
@@ -25,8 +26,13 @@ func main() {
 
 	// config
 	host := flag.String("b", ":9999", "A simple bindhost string, eg: \":9999\" or \"127.0.0.1\"")
-	uri_size := flag.Uint("l", 10, "set the generated uri string length, eg: 10")
-	dash := flag.Uint("-d", 5, "set how often to insert a dash, eg: 5")
+	uri_size := flag.Uint("l", 10, "set the generated uri string length")
+	dash := flag.Uint("d", 5, "set how often to insert a dash")
+	dt, err := time.ParseDuration("10s")
+	if err != nil {
+		dt = time.Second * 10
+	}
+	cache_to := flag.String("c", dt.String(), "set the time in seconds for cache expiry")
 	flag.Parse()
 
 	// golang seed is subpar.
@@ -48,28 +54,54 @@ func main() {
 		}
 		return nil
 	})
+
+	// set up cache watcher
+	go func() {
+		for {
+			err := db.Update(func(tx *bolt.Tx) error {
+				b := tx.Bucket([]byte("gurls"))
+
+				err = b.ForEach(func(k, v []byte) error {
+					if v != nil {
+						var rec record
+						err = json.Unmarshal(v, &rec)
+						if err != nil {
+							return err
+						}
+						ct, err := time.ParseDuration(*cache_to)
+						if err != nil {
+							ct = time.Second * 10
+						}
+						if time.Now().Sub(rec.Last_visit) >= ct {
+							log.Printf("%s has expired @ %d seconds", rec.Key, time.Now().Sub(rec.Last_visit))
+							err = b.Delete([]byte(rec.Key))
+							if err != nil {
+								log.Fatal("couldn't delete key ", err)
+							}
+						}
+					}
+					return nil
+				})
+				return nil
+			})
+			if err != nil {
+				log.Fatalf("watcher died: %#v", err)
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+
 	// simple fasthttp server
 	rtr := router.New()
 	rtr.GET("/", func(ctx *fasthttp.RequestCtx) {
 		fmt.Fprintln(ctx, "Haaaaay, gurl! This is an ultralight url shortener.\nTry /create/your-url!")
 	})
-	// debug stuff
-	// rtr.GET("/view", func(ctx *fasthttp.RequestCtx) {
-	// 	db.View(func(tx *bolt.Tx) error {
-	// 		b := tx.Bucket([]byte("gurls"))
-	// 		err := b.ForEach(func(k, v []byte) error {
-	// 			log.Printf("processing k,v: %s,%s\n", k, v)
-	// 			fmt.Fprintf(ctx, "%s\n\t->%s\n", k, v)
-	// 			return nil
-	// 		})
-	// 		return err
-	// 	})
-	// })
 	rtr.GET("/create", func(ctx *fasthttp.RequestCtx) {
 		fmt.Fprintln(ctx, "Oops! You forgot a trailing /some-url-here after your /create there!")
 	})
 	rtr.GET("/create/{uri}", func(ctx *fasthttp.RequestCtx) {
 		db.Update(func(tx *bolt.Tx) error {
+			// build our key and get uri
 			var k bytes.Buffer
 			for c := uint(0); c <= *uri_size; c++ {
 				// if it's not the first or last
@@ -79,29 +111,39 @@ func main() {
 				}
 				k.WriteRune(alphabet[rand.Intn(len(alphabet))])
 			}
-
 			uri := ctx.UserValue("uri").(string)
-			fmt.Fprintf(ctx, "key: %s\nvalue: %s", k.String(), uri)
+
+			// marshal it
+			rec := record{Last_visit: time.Now(), Key: k.String(), Uri: "https://" + uri}
+			out, err := json.Marshal(rec)
+			if err != nil {
+				log.Fatal("couldn't marshal: ", err)
+			}
 
 			b := tx.Bucket([]byte("gurls"))
-			err := b.Put(k.Bytes(), []byte(uri))
+			err = b.Put(k.Bytes(), out)
+			fmt.Fprint(ctx, string(out))
 			return err
 		})
 	})
 	rtr.GET("/b/{req_uri}", func(ctx *fasthttp.RequestCtx) {
-		var req_uri bytes.Buffer
+		var rec record
 		// assume https because why not
-		req_uri.WriteString("https://")
+		// req_uri.WriteString("https://")
 
 		// build redirect
-		db.View(func(tx *bolt.Tx) error {
+		err = db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("gurls"))
-			req_uri.WriteString(string(b.Get([]byte(ctx.UserValue("req_uri").(string)))))
+			err = json.Unmarshal(b.Get([]byte(ctx.UserValue("req_uri").(string))), &rec)
+			if err != nil {
+				ctx.NotFound()
+				return err
+			}
+
+			// send it
+			ctx.Redirect(rec.Uri, 302)
 			return nil
 		})
-		rtr.RedirectFixedPath = true
-		// send it
-		ctx.Redirect(req_uri.String(), 302)
 	})
 	log.Fatal(fasthttp.ListenAndServe(*host, rtr.Handler))
 }
